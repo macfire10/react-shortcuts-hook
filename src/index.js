fa6e0623c5ccbe6e0839edfe53970ae78efeec1a
evantianx <evantianx@gmail.com>
2019-11-20 10:50:43 +0800

    refactor: improve SetsEqual

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import { useEffect, useCallback } from 'react'
+import { useEffect, useCallback } from 'react';
 
-function setsEqual(setA, setB) {
-  const a = Array.from(setA)
-  const b = Array.from(setB)
-  if (a.length != b.length) return false
-
-  for (let i of a) {
-    if (b.indexOf(i) == -1) return false
-  }
-
-  return true
-}
-
+const setsEqual = (setA, setB) =>
+  setA.size === setB.size && !Array.from(setA).some(v => !setB.has(v));
+  
 export function useShortcuts(keys, callback, deps) {
   const memoizedCallback = useCallback(callback, deps || [])
   const targetKeys = new Set(keys.map(key => key.toLowerCase()))
